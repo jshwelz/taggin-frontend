ecd13194bed1d044e956c1ab8834ac4fc345fe9a
jshwelz <iam_jwlechez@indeed.com>
2020-12-20 23:20:02 -0600

    main code add it

---
+++ README.md
@@ -0,0 +1,34 @@
+## SelectStart Fronted
+
+#### Example:
+
+Tagging
+
+An application used to shows the group of words from the backend.
+
+
+#### Example:
+
+This project is currently in development. Users can filter tweets by username and keyword and see visual data representation. Functionality to sort by additional parameters is in progress.
+
+## Installation and Setup Instructions
+
+
+
+Clone down this repository. You will need `node` and `npm` installed globally on your machine.  
+
+Installation:
+
+`npm install`  
+
+To Run Test Suite:  
+
+`npm test`  
+
+To Start Server:
+
+`npm start`  
+
+To Visit App:
+
+`localhost:3000`  
---
+++ src/demo.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
+
+const useStyles = makeStyles({
+  root: {
+    height: 110,
+    flexGrow: 1,
+    maxWidth: 400,
+  },
+});
+
+export default function RecursiveTreeView() {
+  const classes = useStyles();
+  const [tags, setTags] = useState([])
+  
+  
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/tags/").then((response) =>
+      response.json()
+    ).then((data) => {
+      setTags(data)      
+    }
+    )
+  }, [])
+
+  const renderTree = (nodes) => (
+    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+      {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
+    </TreeItem>
+  );
+
+  return (
+    <TreeView
+      className={classes.root}
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpanded={['root']}
+      defaultExpandIcon={<ChevronRightIcon />}
+    >
+      {renderTree(tags)}
+    </TreeView>
+  );
+}
